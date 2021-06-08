5c7ffb129fc7fc55d04a2097f45a013a960c22ad
jack <workingjack91@naver.com>
2021-06-08 23:56:07 +0900

    프로젝트생성

---
+++ src/OpenChallenge.java
@@ -0,0 +1,9 @@
+
+public class OpenChallenge {
+	public static void main(String[] args) {
+		System.out.println("Kitae Hwang");
+		System.out.println("20 years old");
+		System.out.println("Department of Computer Engineering");
+	}
+}
+  
---
+++ src/Practice_P55.java
@@ -0,0 +1,13 @@
+
+public class Practice_P55 {
+   public static void main(String[] args) {
+	   System.out.println("[실습문제1]");
+	   System.out.println("Welcome!!");
+	   System.out.println("자바 세계로 오신 것을 환영합니다.");
+	   
+	   System.out.println();
+	   System.out.println("[실습문제2]");
+	   System.out.println("Sorry~~~~");
+	   System.out.println("재밌어서 미안합니다~~");
+   }
+}
